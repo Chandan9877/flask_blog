b20a1d9603a391c9e37add30e8a16a9fd11956d1
chandan9877 <chandanmahato.com@gmail.com>
2026-07-15 09:29:35 +0545

    restructured everything

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,45 +1,16 @@
+from flaskblog.model import User,Post
+from flaskblog import app 
 from flask import Flask, render_template, url_for, flash,redirect
-from forms import RegistrationForm,LoginForm
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'fjoijfoaeif234324hj3h2k4233'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20),unique=True,nullable = False)
-    email = db.Column(db.String(120),unique=True,nullable = False)
-    image_file = db.Column(db.String(20),unique=True,nullable = False, default = 'default.jpg')
-    password = db.Column(db.String(100),nullable = False)
-    posts = db.relationship('Post',backref='author',lazy = True)
-
-    def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer,primary_key = True)
-    title = db.Column(db.String(100),nullable = False)
-    date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-    content = db.Column(db.Text,nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-     
-    def __repr__(self):
-        return f"Post('{self.title}','{self.date_posted}')"
-
+from flaskblog.forms import RegistrationForm,LoginForm
 
 posts = [
     {
         'author' : 'Corey Schafer',
         'title' : "Blog post 1",
-        'content' : 'First post content',
+        'content' : 'First post content', 
         'date_posted' : 'jul 10 , 2026'
     },
-    {
+    { 
         'author' : 'Jane Doe',
         'title' : "Blog post 2",
         'content' : 'Second post content',
@@ -79,6 +50,3 @@ def login():
         else:
             flash("Login Uncessful. please check username and password",'danger')
     return render_template('login.html',title = 'Login',form = form)
-
-if __name__ == "__main__":
-    app.run(debug=True)
